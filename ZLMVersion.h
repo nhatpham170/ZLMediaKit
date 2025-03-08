d5252d54ba95df109f6c6c8cf46b9019709e7507
nhatpham170 <nhatpham170@gmail.com>
2025-03-08 19:35:02 +0700

    build for windows

--- ZLMVersion.h
+++ ZLMVersion.h
@@ -1,10 +1,10 @@
 #ifndef __GIT_VERSION_H__
 #define __GIT_VERSION_H__
 
-#define COMMIT_HASH "52ca731"
-#define COMMIT_TIME "2025-02-26T11:32:10+08:00"
+#define COMMIT_HASH "2c8d816"
+#define COMMIT_TIME "2025-03-05T00:29:44+07:00"
 #define BRANCH_NAME "master"
-#define BUILD_TIME "2025-03-04T21:28:05"
+#define BUILD_TIME "2025-03-06T00:14:19"
 
 #endif //__GIT_VERSION_H__
 
